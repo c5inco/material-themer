1798a06d1b7d89e5e77a83fbf55eaa683878e620
Chris Sinco <csinco.design@gmail.com>
2024-07-30 00:21:45 -0700

    Add color switcher

--- iosApp/iosApp/SampleFormView.swift
+++ iosApp/iosApp/SampleFormView.swift
@@ -1,6 +1,21 @@
 import SwiftUI
 import ComposeApp
 
+let seedColors = [
+    KotlinColor(red: 0.7019607843, green: 0.231372549, blue: 0.08235294118, alpha: 1.0),
+    KotlinColor(red: 0.3882352941, green: 0.6274509804, blue: 0.007843137255, alpha: 1.0),
+    KotlinColor(red: 0.462745098, green: 0.6117647059, blue: 0.8745098039, alpha: 1.0),
+    KotlinColor(red: 1.0, green: 0.8705882353, blue: 0.2470588235, alpha: 1.0),
+]
+
+func toSwiftUiColor(kotlinColor: KotlinColor) -> Color {
+    return Color(
+        red: kotlinColor.red,
+        green: kotlinColor.green,
+        blue: kotlinColor.blue
+    )
+}
+
 struct SampleFormView: View {
     @Environment(\.colorScheme) var iosColorScheme
     @State var viewModel: ThemeViewModel
@@ -10,23 +25,17 @@ struct SampleFormView: View {
     @State var stepper = 1
     @State var name = ""
     @State var colors = ["Red", "Green", "Blue"]
-    let seedColors = [
-        KotlinColor(red: 0.7019607843, green: 0.231372549, blue: 0.08235294118, alpha: 1.0),
-        KotlinColor(red: 0.3882352941, green: 0.6274509804, blue: 0.007843137255, alpha: 1.0),
-        KotlinColor(red: 0.462745098, green: 0.6117647059, blue: 0.8745098039, alpha: 1.0),
-        KotlinColor(red: 1.0, green: 0.8705882353, blue: 0.2470588235, alpha: 1.0),
-    ]
+    @State var activeSeedColor: KotlinColor = seedColors[0]
     @State var seedIndex = 0
     
     var body: some View {
         let colorScheme = viewModel.getDynamicColorScheme(
-            seedColor: seedColors[seedIndex],
+            seedColor: activeSeedColor,
             isDark: iosColorScheme == .dark
         )
         let surface = colorScheme.surface
         let surfaceContainer = colorScheme.surfaceContainer
         let primary = colorScheme.primary
-        let secondary = colorScheme.secondary
         
         TabView {
             Group {
@@ -79,7 +88,7 @@ struct SampleFormView: View {
                                     Toggle(isOn: $enableLogging) {
                                         Text("Title")
                                     }
-                                    .tint(toSwiftUiColor(kotlinColor: secondary))
+                                    .tint(toSwiftUiColor(kotlinColor: colorScheme.tertiary))
                                 }
                                 .listRowBackground(toSwiftUiColor(kotlinColor: surfaceContainer))
                                 
@@ -88,24 +97,9 @@ struct SampleFormView: View {
                             .scrollContentBackground(.hidden)
                         }
                         .toolbar {
-                            ToolbarItem(placement: .primaryAction) {
-                                Button(action: {
-                                    seedIndex = 2
-                                }) {
-                                    Image(systemName: "visionpro")
-                                        .imageScale(.large)
-                                }
-                            }
-                            ToolbarItem(placement: .topBarLeading) {
-                                Button(action: {
-                                    seedIndex = 3
-                                }) {
-                                    Image(systemName: "person.crop.circle")
-                                        .imageScale(.large)
-                                }
-                            }
+                            ToolbarView(action: updateActiveSeedColor, selectedColor: activeSeedColor)
                         }
-                        .navigationTitle("Inset Lists")
+                        .navigationTitle("Sample form")
                         .background(toSwiftUiColor(kotlinColor: surface))
                     }
                 }.tabItem {
@@ -131,12 +125,48 @@ struct SampleFormView: View {
         .accentColor(toSwiftUiColor(kotlinColor: primary))
     }
     
-    private func toSwiftUiColor(kotlinColor: KotlinColor) -> Color {
-        return Color(
-            red: kotlinColor.red,
-            green: kotlinColor.green,
-            blue: kotlinColor.blue
-        )
+    func updateActiveSeedColor(color: KotlinColor) {
+        print(color)
+        activeSeedColor = color
+    }
+}
+
+struct ToolbarView: ToolbarContent {
+    let action: (KotlinColor) -> Void
+    @State var selectedColor: KotlinColor
+
+    init(action: @escaping (KotlinColor) -> Void, selectedColor: KotlinColor) {
+        self.action = action
+        self.selectedColor = selectedColor
+    }
+    
+    var body: some ToolbarContent {
+        ToolbarItem(placement: .primaryAction) {
+            Menu {
+                Picker("Color Palette", selection: $selectedColor) {
+                    ForEach(seedColors, id: \.self) { seedColor in
+                        Image(systemName: "circle.fill")
+                            .tint(toSwiftUiColor(kotlinColor: seedColor))
+                    }
+                  }
+                  .pickerStyle(.palette)
+                  .menuActionDismissBehavior(.enabled)
+                  .onChange(of: selectedColor) { _, newColor in
+                      action(newColor)
+                  }
+            } label: {
+                Image(systemName: "swatchpalette.fill")
+                    .imageScale(.large)
+            }
+        }
+        ToolbarItem(placement: .topBarLeading) {
+            Button(action: {
+                
+            }) {
+                Image(systemName: "person.crop.circle")
+                    .imageScale(.large)
+            }
+        }
     }
 }
 
