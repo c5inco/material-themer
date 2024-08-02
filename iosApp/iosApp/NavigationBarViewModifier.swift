04a47da80ce49555d594fc9081a6820704406663
Chris Sinco <csinco@google.com>
2024-08-02 11:37:24 -0700

    Cleanup

--- iosApp/iosApp/NavigationBarViewModifier.swift
+++ iosApp/iosApp/NavigationBarViewModifier.swift
@@ -9,25 +9,27 @@
 import SwiftUI
 
 struct NavigationBarViewModifier: ViewModifier {
-    init(backgroundColor: UIColor = .systemBackground, foregroundColor: UIColor = .blue, tintColor: UIColor? = nil) {
-            let navBarAppearance = UINavigationBarAppearance()
-            navBarAppearance.titleTextAttributes = [.foregroundColor: foregroundColor]
-            navBarAppearance.largeTitleTextAttributes = [.foregroundColor: foregroundColor]
-            UINavigationBar.appearance().standardAppearance = navBarAppearance
+    init(
+        backgroundColor: UIColor = .systemBackground,
+        foregroundColor: UIColor = .blue,
+        tintColor: UIColor? = nil
+    ) {
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.titleTextAttributes = [.foregroundColor: foregroundColor]
+        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: foregroundColor]
+        UINavigationBar.appearance().standardAppearance = navBarAppearance
+    
+        let scrollNavBarAppearance = UINavigationBarAppearance()
+        scrollNavBarAppearance.titleTextAttributes = [.foregroundColor: foregroundColor]
+        scrollNavBarAppearance.largeTitleTextAttributes = [.foregroundColor: foregroundColor]
+        scrollNavBarAppearance.backgroundColor = backgroundColor
+        scrollNavBarAppearance.shadowColor = .clear
+        UINavigationBar.appearance().scrollEdgeAppearance = scrollNavBarAppearance
         
-//            UINavigationBar.appearance().compactAppearance = navBarAppearance
-        
-            let scrollNavBarAppearance = UINavigationBarAppearance()
-            scrollNavBarAppearance.titleTextAttributes = [.foregroundColor: foregroundColor]
-            scrollNavBarAppearance.largeTitleTextAttributes = [.foregroundColor: foregroundColor]
-            scrollNavBarAppearance.backgroundColor = backgroundColor
-            scrollNavBarAppearance.shadowColor = .clear
-            UINavigationBar.appearance().scrollEdgeAppearance = scrollNavBarAppearance
-            
-            if let tintColor = tintColor {
-                UINavigationBar.appearance().tintColor = tintColor
-            }
+        if let tintColor = tintColor {
+            UINavigationBar.appearance().tintColor = tintColor
         }
+    }
     
     func body(content: Content) -> some View {
         content
