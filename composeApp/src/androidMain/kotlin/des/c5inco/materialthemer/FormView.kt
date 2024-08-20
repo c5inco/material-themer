b9a3301ed722d3bcf7797b5a73880dc308b3e71d
Chris Sinco <csinco.design@gmail.com>
2024-08-19 23:45:53 -0700

    New Form view

--- composeApp/src/androidMain/kotlin/des/c5inco/materialthemer/FormView.kt
+++ composeApp/src/androidMain/kotlin/des/c5inco/materialthemer/FormView.kt
@@ -1,15 +1,22 @@
 package des.c5inco.materialthemer
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
 import des.c5inco.materialthemer.ui.theme.AppTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -22,17 +29,39 @@ fun FormView(
     Scaffold(
         topBar = {
             MediumTopAppBar(
-                title = { Text("Items",) },
-                navigationIcon =  { },
+                title = { Text("Items") },
+                navigationIcon =  {
+                    IconButton(onClick = {}) {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_hmd),
+                            contentDescription = null
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = {}) {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_palette),
+                            contentDescription = null
+                        )
+                    }
+                },
                 scrollBehavior = scrollBehavior
             )
         },
         modifier = modifier
     ) { innerPadding ->
         Column(
-            modifier = Modifier.padding(innerPadding)
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(horizontal = 16.dp)
         ) {
-            Text("Form")
+            Button(
+                onClick = {},
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Action")
+            }
         }
     }
 }
