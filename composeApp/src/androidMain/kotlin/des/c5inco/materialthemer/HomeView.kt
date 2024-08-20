96f15bf4f797fd769c91cb55f273fbdb9a09d1f4
Chris Sinco <csinco.design@gmail.com>
2024-08-19 23:14:16 -0700

    New Home View for native Android app

---
+++ composeApp/src/androidMain/kotlin/des/c5inco/materialthemer/HomeView.kt
@@ -0,0 +1,93 @@
+package des.c5inco.materialthemer
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+
+sealed class Screen(
+    val route: String,
+    val name: String,
+    @DrawableRes val resourceId: Int
+) {
+    object Form : Screen("form", "Form", R.drawable.ic_settings)
+    object Cards : Screen("cards", "Cards", R.drawable.ic_card)
+    object Palette : Screen("palette", "Palette", R.drawable.ic_stacks)
+}
+
+val navigationItems = listOf(
+    Screen.Form, Screen.Cards, Screen.Palette
+)
+
+@Composable
+fun HomeView(
+    modifier: Modifier = Modifier
+) {
+    val navController = rememberNavController()
+
+    Scaffold(
+        bottomBar = {
+            NavigationBar {
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentDestination = navBackStackEntry?.destination
+
+                navigationItems.forEach { screen ->
+                    NavigationBarItem(
+                        icon = { Icon(
+                            painter = painterResource(id = screen.resourceId),
+                            contentDescription = null)
+                        },
+                        label = { Text(screen.name) },
+                        selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                        onClick = {
+                            navController.navigate(screen.route) {
+                                popUpTo(navController.graph.findStartDestination().id) {
+                                    saveState = true
+                                }
+                                launchSingleTop = true
+                                restoreState = true
+                            }
+                        }
+                    )
+                }
+            }
+        }
+    ) { innerPadding ->
+        // Content of your screen
+        NavHost(
+            navController = navController,
+            startDestination = Screen.Form.route,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(Screen.Form.route) {
+                Text("Form")
+            }
+            composable(Screen.Cards.route) {
+                Text("Cards")
+            }
+            composable(Screen.Palette.route) {
+                Text("Palette")
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun HomeViewPreview() {
+    HomeView()
+}
