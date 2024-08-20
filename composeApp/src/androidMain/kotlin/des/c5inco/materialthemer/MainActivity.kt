faa88942aadde93521ff4be05712eb34c1f3b335
Chris Sinco <csinco.design@gmail.com>
2024-08-19 23:32:25 -0700

    Use dynamic color theming for Android

--- composeApp/src/androidMain/kotlin/des/c5inco/materialthemer/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/des/c5inco/materialthemer/MainActivity.kt
@@ -1,12 +1,17 @@
 package des.c5inco.materialthemer
 
-import App
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.materialkolor.DynamicMaterialTheme
+import com.materialkolor.PaletteStyle
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +19,24 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
 
         setContent {
-            HomeView()
+            val useDarkIcons = isSystemInDarkTheme()
+            DisposableEffect(useDarkIcons) {
+                enableEdgeToEdge(
+                    statusBarStyle = SystemBarStyle.auto(
+                        android.graphics.Color.TRANSPARENT,
+                        android.graphics.Color.TRANSPARENT,
+                    ) { useDarkIcons },
+                )
+                onDispose {}
+            }
+
+            DynamicMaterialTheme(
+                seedColor = Color.Red,
+                useDarkTheme = useDarkIcons,
+                style = PaletteStyle.TonalSpot
+            ) {
+                HomeView()
+            }
         }
     }
 }
